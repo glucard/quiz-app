ed5631163177e0e1a1238f38943e1c87a2776117
glucard <glucas11@hotmail.com>
2023-09-30 16:13:53 -0400

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,39 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
+import { useGlobalContext } from "./not_sure";
 
 export default function Home() {
+
+  function start_handler() {
+    let testando = useGlobalContext;
+    console.log(testando());
+    console.log("Aasdasdasokosk")
+  }
+
   return (
     <main className="flex flex-col items-center gap-3">
+        <p className="text-white text-lg px-5 text-center">
+          Teste seus conhecimentos sobre Ciência da Computação!
+        </p>
 
-      <p className="text-white text-lg px-5 text-center">
-        Teste seus conhecimentos sobre Ciência da Computação!
-      </p>
-      <Link href="/quiz">
-        <button className="bg-orange-400 hover:bg-orange-600 font-bold py-2 px-4 rounded-full">
-          Start Quiz
-        </button>
-      </Link>
+        <div className="flex flex-col gap-3 items-center">
+          <div>
+            <label htmlFor="username">seu nome: </label>
+            <input name="username" />
+          </div>
+          <div>
+            <label htmlFor="email">seu e-mail: </label>
+            <input name="email" />
+          </div>
+        </div>
+
+        <Link href="/quiz">
+          <button onClick={start_handler} className="bg-orange-400 hover:bg-orange-600 font-bold py-2 px-4 rounded-full">
+            Start Quiz
+          </button>
+        </Link>
     </main>
   );
 }
